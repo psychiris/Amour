d5ac4e8c59d88333b38ee6314ac76e5e19b944b3
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2025-11-18 00:59:16 +0300

    [Tweak] Unchudify Time System (#176)

--- Content.Shared/_Orion/Time/TimeSystem.cs
+++ Content.Shared/_Orion/Time/TimeSystem.cs
@@ -1,3 +1,6 @@
+using Content.Shared.CCVar;
+using Robust.Shared.Configuration;
+
 namespace Content.Shared._Orion.Time;
 
 //
@@ -6,12 +9,37 @@ namespace Content.Shared._Orion.Time;
 
 public sealed class TimeSystem : EntitySystem
 {
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+
+    private int _yearOffset;
+    private int _staticYear;
+    private bool _useStaticYear;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        _cfg.OnValueChanged(CCVars.StationTimeOffsetYears, v => _yearOffset = v, true);
+        _cfg.OnValueChanged(CCVars.StationTimeUseStaticYear, v => _useStaticYear = v, true);
+        _cfg.OnValueChanged(CCVars.StationTimeStaticYear, v => _staticYear = v, true);
+    }
+
     public DateTime GetStationDate()
     {
         var today = DateTime.UtcNow.Date;
-        var futureYear = today.Year + 500;
-        var day = Math.Min(today.Day, DateTime.DaysInMonth(futureYear, today.Month));
-        var stationDate = new DateTime(futureYear, today.Month, day);
+
+        int stationYear;
+        if (_useStaticYear)
+        {
+            stationYear = _staticYear; // Static year
+        }
+        else
+        {
+            stationYear = today.Year + _yearOffset; // Dynamic year
+        }
+
+        var day = Math.Min(today.Day, DateTime.DaysInMonth(stationYear, today.Month));
+        var stationDate = new DateTime(stationYear, today.Month, day);
 
         return stationDate;
     }
