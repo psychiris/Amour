ee9785611096aa9986768c19a31d3d8e8237a208
kuamorg <anon12341242345@gmail.com>
2026-01-17 21:10:36 +0300

    Индикатор боевого режима на персонаже

--- Content.Client/Options/UI/Tabs/AccessibilityTab.xaml.cs
+++ Content.Client/Options/UI/Tabs/AccessibilityTab.xaml.cs
@@ -68,7 +68,8 @@ public sealed partial class AccessibilityTab : Control
 
         Control.AddOptionCheckBox(CCVars.ChatEnableColorName, EnableColorNameCheckBox);
         Control.AddOptionCheckBox(CCVars.AccessibilityColorblindFriendly, ColorblindFriendlyCheckBox);
-        Control.AddOptionCheckBox(CCVars.CombatIndicator, CombatIndicatorCheckBox); // Orion
+        // Amour edit
+        // Control.AddOptionCheckBox(CCVars.CombatIndicator, CombatIndicatorCheckBox); // Orion
         Control.AddOptionCheckBox(CCVars.ReducedMotion, ReducedMotionCheckBox);
         Control.AddOptionPercentSlider(CCVars.ScreenShakeIntensity, ScreenShakeIntensitySlider);
         Control.AddOptionPercentSlider(CCVars.ChatWindowOpacity, ChatWindowOpacitySlider);
